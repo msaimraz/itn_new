26805fad36a8f21daa8c1988ca140af4632a5b34
Sanjay Andani <126229750+SanjayAndani@users.noreply.github.com>
2025-11-30 02:13:21 +0500

    pages

--- data/casesData.js
+++ data/casesData.js
@@ -16,16 +16,11 @@ export const CASES = [
 
     tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
     categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
-    topImg: "/webImages/cases-new/brasaochbasilika.png",
-    innerImg: "/webImages/cases/budo-webb-1-680x480.jpg",
-    hero: "/webImages/cases-new/brasaochbasilika-banner.png",
+    topImg: "/webImages/cases-new/brasaochbasilika/brasaochbasilika.png",
+    innerImg: "/webImages/cases-new/brasaochbasilika/brasaochbasilikatop.png",
+    hero: "/webImages/cases-new/brasaochbasilika/brasaochbasilika-banner.png",
     sidebarr: {
-      vad: [
-        "UX Design",
-        "Web Development",
-        "Product presentation",
-        "SEO"
-      ],
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
     },
 
     sectionone: {
@@ -50,29 +45,345 @@ export const CASES = [
       },
       {
         title: "Visit",
-        link: "https://brasaochbasilika.se/",
+        link: "brasaochbasilika.se/",
       },
     ],
 
-
     sectiontwo: {
-      img: "/webImages/cases-new/brasaochbasilika-3sec.png",
-      heading:
-        "Brasa & Basilika - Details",
+      img: "/webImages/cases-new/brasaochbasilika/brasaochbasilika-3sec.png",
+      heading: "Brasa & Basilika - Details",
       text: `
           Brasa & Basilika – A restaurant that combines the raw flavors of the fire with the fresh herbs of the Mediterranean. We developed a modern, tasteful design and created a vibrant menu with a focus on grilled ingredients, wood-fired pizza and hand-picked wines. The result was a digital presence that matches the restaurant’s warm atmosphere and culinary experience.
         `,
     },
 
     sectionthree: {
-      img: "/webImages/cases-new/brasaochbasilika-3sec.png",
-      heading:
-        "Brasa & Basilika - Details",
+      img: "/webImages/cases-new/brasaochbasilika/brasaochbasilika.png",
+      heading: "Brasa & Basilika - Details",
       text: `
           Brasa & Basilika – A restaurant that combines the raw flavors of the fire with the fresh herbs of the Mediterranean. We developed a modern, tasteful design and created a vibrant menu with a focus on grilled ingredients, wood-fired pizza and hand-picked wines. The result was a digital presence that matches the restaurant’s warm atmosphere and culinary experience.
         `,
     },
   },
+  {
+    slug: "tallcohtimjan",
+    title: "Pine & Thyme",
 
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/tallcohtimjan/tallcohtimjan1.png",
+    innerImg: "/webImages/cases-new/tallcohtimjan/tallcohtimjantop.png",
+    hero: "/webImages/cases-new/tallcohtimjan/tallcohtimjan-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
 
+    sectionone: {
+      type: "intro",
+      heading: "Tall & Timjan opens the doors to their new unique website",
+      text: `
+          Tall & Timjan – A restaurant where Nordic ingredients meet Mediterranean flair in a seasonal menu shaped by the chef’s personality. We created a website design that reflects the warm atmosphere, the open fireplace and the culinary care – with a vibrant menu that highlights game, sea and grill in harmony.        `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Pine & Thyme",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://tallcohtimjan.se",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/tallcohtimjan/tallcohtimjan3.png",
+      heading: "Pine & Thyme – details",
+      text: `
+      Tall & Timjan – A restaurant where Nordic ingredients meet Mediterranean flair in a seasonal menu shaped by the chef’s personality. We created a website design that reflects the warm atmosphere, the open fireplace and the culinary care – with a vibrant menu that highlights game, sea and grill in harmony.        `,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/tallcohtimjan/tallcohtimjan1.png",
+      heading: "Responsive design",
+      text: `
+      Tall & Timjan – A restaurant where Nordic ingredients meet Mediterranean flair in a seasonal menu shaped by the chef’s personality. We created a website design that reflects the warm atmosphere, the open fireplace and the culinary care – with a vibrant menu that highlights game, sea and grill in harmony.        `,
+    },
+  },
+
+  {
+    slug: "mato-mojo",
+    title: "Mato Mojo",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/Mato-Mojo/Mato-Mojo-banner.png",
+    innerImg: "/webImages/cases-new/Mato-Mojo/matomojotop.png",
+    hero: "/webImages/cases-new/Mato-Mojo/Mato-Mojo-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading: "Mato Mojo opens the doors to its new unique website",
+      text: `
+        Mato Mojo – A modern food concept where street food meets the flavors of the world. We created a colorful and energetic design that matches the menu's bowls, wraps, dumplings and hot sauces. The result? A digital identity that conveys the feeling of speed, flavor explosions and a really good vibe.   `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Mato Mojo",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://matomojo.se",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/Mato-Mojo/matomojo3.png",
+      heading: "Mato Mojo – details",
+      text: `
+      Mato Mojo – A modern food concept where street food meets the flavors of the world. We created a colorful and energetic design that matches the menu's bowls, wraps, dumplings and hot sauces. The result? A digital identity that conveys the feeling of speed, flavor explosions and a really good vibe.
+`,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/Mato-Mojo/matomojo.png",
+      heading: "Responsive design",
+      text: `
+      Mato Mojo – A modern food concept where street food meets the flavors of the world. We created a colorful and energetic design that matches the menu's bowls, wraps, dumplings and hot sauces. The result? A digital identity that conveys the feeling of speed, flavor explosions and a really good vibe.       `,
+    },
+  },
+
+  {
+    slug: "mindwell-psykologi",
+    title: "MindWell Psychology",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/MindWell/mindwell-banner.png",
+    innerImg: "/webImages/cases-new/MindWell/mindwelltop.png",
+    hero: "/webImages/cases-new/MindWell/mindwell-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading: "MindWell Psychology opens the doors to its new unique website",
+      text: `
+        MindWell Psykologi AB – A private psychology practice in central Gothenburg that offers CBT for both individuals and couples. We created a calm and trust-building design that reflects the professionalism and warmth of the business. With a focus on clear information and a secure structure, we highlighted both the therapeutic offering and business services such as supervision and organizational development.  `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "MindWell Psychology",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://mindwell.nu",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/MindWell/mindwell3.png",
+      heading: "MindWell Psychology - details",
+      text: `
+      MindWell Psykologi AB – A private psychology practice in central Gothenburg that offers CBT for both individuals and couples. We created a calm and trust-building design that reflects the professionalism and warmth of the business. With a focus on clear information and a secure structure, we highlighted both the therapeutic offering and business services such as supervision and organizational development.
+`,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/MindWell/mindwell.png",
+      heading: "Responsive design",
+      text: `
+      MindWell Psykologi AB – A private psychology practice in central Gothenburg that offers CBT for both individuals and couples. We created a calm and trust-building design that reflects the professionalism and warmth of the business. With a focus on clear information and a secure structure, we highlighted both the therapeutic offering and business services such as supervision and organizational development.     `,
+    },
+  },
+
+  {
+    slug: "talentra-hr-konsult-ab",
+    title: "Talentera HR Consultant AB",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/talentra/talentra-banner.png",
+    innerImg: "/webImages/cases-new/talentra/talentratop.png",
+    hero: "/webImages/cases-new/talentra/talentra-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading:
+        "Talentera HR Konsult AB opens the doors to its new unique website",
+      text: `
+        Talentera HR Konsult AB – We offer strategic HR services, recruitment and interim solutions for companies in change. With solid experience from leading HR roles, we create tailored solutions in leadership development, employer branding and HR strategy – always in close collaboration with our clients. `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Talentera HR Consultant AB",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://talentera.se/",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/talentra/talentera3.png",
+      heading: "Talentera HR Konsult AB – details",
+      text: `
+      Talentera HR Konsult AB – We offer strategic HR services, recruitment and interim solutions for companies in change. With solid experience from leading HR roles, we create tailored solutions in leadership development, employer branding and HR strategy – always in close collaboration with our clients.`,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/talentra/talentera.png",
+      heading: "Responsive design",
+      text: `
+      Talentera HR Konsult AB – We offer strategic HR services, recruitment and interim solutions for companies in change. With solid experience from leading HR roles, we create tailored solutions in leadership development, employer branding and HR strategy – always in close collaboration with our clients.    `,
+    },
+  },
+
+  {
+    slug: "tryggahjulet",
+    title: "Safe Wheel Car & Tires AB",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/tryggahjulet/tryggahjulet-banner.png",
+    innerImg: "/webImages/cases-new/tryggahjulet/tryggahjulet-top.png",
+    hero: "/webImages/cases-new/tryggahjulet/tryggahjulet-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading:
+        "Talentera HR Konsult AB opens the doors to its new unique website",
+      text: `
+        Trygga Hjulet Bil & Däck AB – Full-service workshop that offers tires, rims and car accessories for both private individuals and companies. We designed a new website with an integrated booking function that makes it easy for customers to book service, tire changes and other workshop services – quickly and easily, all year round. `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Safe Wheel Car & Tires AB",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://tryggahjulet.se/",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/tryggahjulet/tryggahjulet3.png",
+      heading: "Trygga Hjulet Bil & Däck AB – details",
+      text: `
+      Trygga Hjulet Bil & Däck AB – Full-service workshop that offers tires, rims and car accessories for both private individuals and companies. We designed a new website with an integrated booking function that makes it easy for customers to book service, tire changes and other workshop services – quickly and easily, all year round.`,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/tryggahjulet/tryggahjulet.png",
+      heading: "Responsive design",
+      text: `
+      Trygga Hjulet Bil & Däck AB – Full-service workshop that offers tires, rims and car accessories for both private individuals and companies. We designed a new website with an integrated booking function that makes it easy for customers to book service, tire changes and other workshop services – quickly and easily, all year round.   `,
+    },
+  },
+
+  {
+    slug: "norlux-gear",
+    title: "Norlux Gear",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/norlux-gear/norluxgear-banner.png",
+    innerImg: "/webImages/cases-new/norlux-gear/norluxgear-top.png",
+    hero: "/webImages/cases-new/norlux-gear/norluxgear-banner.png",
+    sidebarr: {
+      vad: ["UX Design", "Web Development", "Product presentation", "SEO"],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading: "Norlux Gear opens the doors to its new unique website",
+      text: `
+        Norlux Gear – The brand where Scandinavian design meets technical performance in clothing and equipment for both city and wilderness. We created a modern e-commerce solution with a smooth CMS that makes it easy to manage assortment, content and campaigns – tailored for a stylish and adventure-ready customer experience. `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Backstage Hotel",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://norluxgear.se/",
+      },
+    ],
+
+    sectiontwo: {
+      img: "/webImages/cases-new/norlux-gear/norluxgear3.png",
+      heading: "Norlux Gear – details",
+      text: `
+      Norlux Gear – The brand where Scandinavian design meets technical performance in clothing and equipment for both city and wilderness. We created a modern e-commerce solution with a smooth CMS that makes it easy to manage assortment, content and campaigns – tailored for a stylish and adventure-ready customer experience.`,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/norlux-gear/norluxgear.png",
+      heading: "Responsive design",
+      text: `
+      Norlux Gear – The brand where Scandinavian design meets technical performance in clothing and equipment for both city and wilderness. We created a modern e-commerce solution with a smooth CMS that makes it easy to manage assortment, content and campaigns – tailored for a stylish and adventure-ready customer experience.  `,
+    },
+  },
 ];
