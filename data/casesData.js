32fbd47010e1dbcfa345284784f5b930a39e3f42
Arish Ali <109577410+arishhh@users.noreply.github.com>
2025-11-29 16:22:00 +0500

    Mostly images are changed

--- data/casesData.js
+++ data/casesData.js
@@ -73,6 +73,69 @@ export const CASES = [
         `,
     },
   },
+    {
+    slug: "tall-timjan",
+    title: "Tall & Timjan",
+
+    tags: ["API", "Backend", "E-Handel", "Frontend", "UX & UI-Design"],
+    categories: ["api", "backend", "e-handel", "frontend", "ux-ui-design"],
+    topImg: "/webImages/cases-new/brasaochbasilika.png",
+    innerImg: "/webImages/cases/budo-webb-1-680x480.jpg",
+    hero: "/webImages/cases-new/brasaochbasilika-banner.png",
+    sidebarr: {
+      vad: [
+        "UX Design",
+        "Web Development",
+        "Product presentation",
+        "SEO"
+      ],
+    },
+
+    sectionone: {
+      type: "intro",
+      heading: "Brasa & Basilika opens the doors to its new unique website",
+      text: `
+          Brasa & Basilika – A restaurant that combines the raw flavors of the fire with the fresh herbs of the Mediterranean. We developed a modern, tasteful design and created a vibrant menu with a focus on grilled ingredients, wood-fired pizza and hand-picked wines. The result was a digital presence that matches the restaurant’s warm atmosphere and culinary experience.
+        `,
+    },
+
+    Sidebar: [
+      {
+        title: "Customer",
+        text: "Brasa & Basilika",
+      },
+      {
+        title: "When",
+        year: "2025",
+      },
+      {
+        title: "Services",
+      },
+      {
+        title: "Visit",
+        link: "https://brasaochbasilika.se/",
+      },
+    ],
+
+
+    sectiontwo: {
+      img: "/webImages/cases-new/brasaochbasilika-3sec.png",
+      heading:
+        "Brasa & Basilika - Details",
+      text: `
+          Brasa & Basilika – A restaurant that combines the raw flavors of the fire with the fresh herbs of the Mediterranean. We developed a modern, tasteful design and created a vibrant menu with a focus on grilled ingredients, wood-fired pizza and hand-picked wines. The result was a digital presence that matches the restaurant’s warm atmosphere and culinary experience.
+        `,
+    },
+
+    sectionthree: {
+      img: "/webImages/cases-new/brasaochbasilika-3sec.png",
+      heading:
+        "Brasa & Basilika - Details",
+      text: `
+          Brasa & Basilika – A restaurant that combines the raw flavors of the fire with the fresh herbs of the Mediterranean. We developed a modern, tasteful design and created a vibrant menu with a focus on grilled ingredients, wood-fired pizza and hand-picked wines. The result was a digital presence that matches the restaurant’s warm atmosphere and culinary experience.
+        `,
+    },
+  },
 
 
 ];
